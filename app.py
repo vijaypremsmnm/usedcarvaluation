754f4c7ee59474e39df84424a38a7a04fc080ce4
vijaypremsmnm <67164139+vijaypremsmnm@users.noreply.github.com>
2020-07-03 11:00:33 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -9,8 +9,7 @@ app = Flask(__name__)
 model = pickle.load(open('random_forest_regression_model.pkl', 'rb'))
 @app.route('/',methods=['GET'])
 def Home():
-    return render_template('index1.html')
-
+    return render_template('index.html')
 
 standard_to = StandardScaler()
 @app.route("/predict", methods=['POST'])
@@ -43,11 +42,11 @@ def predict():
         prediction=model.predict([[Present_Price,Kms_Driven2,Owner,Year,Fuel_Type_Diesel,Fuel_Type_Petrol,Seller_Type_Individual,Transmission_Mannual]])
         output=round(prediction[0],2)
         if output<0:
-            return render_template('index.html',prediction_texts="Sorry you cannot sell this car")
+            return render_template('Predict.html',prediction_texts="Sorry you cannot sell this car")
         else:
-            return render_template('index.html',prediction_text="You Can Sell The Car at {}".format(output))
+            return render_template('Predict.html',prediction_text="You Can Sell The Car at {}".format(output))
     else:
-        return render_template('index.html')
+        return render_template('Predict.html')
 
 if __name__=="__main__":
     app.run(debug=True,use_reloader=False)
